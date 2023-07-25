eca3295f99ccc92e13399e6a05e5af2370e26019
sunhy469 <2635254982@qq.com>
2023-07-25 18:19:49 +0800

    登录跳转到Home,缓存返回值中的 body 到 localstorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,29 @@
-import React, { useState } from 'react';
+//@ts-nocheck
+import React, {useState} from 'react';
 import Login from './components/Login';
 import Home from './pages/home';
-import { BrowserRouter as Router, 
-  Routes, 
-  Route, 
-  Link,  
-  useNavigate,
-  Navigate
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Link,
+    useNavigate,
+    Navigate
 } from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0);
+    const [count, setCount] = useState(0);
 
-  return (
-    // <div>
-    //   <Login />
-    // </div>
-    <Router>
-      <Routes>
-        <Route exact path='/' element={<Login />}></Route>
-        <Route  path='/home' element={<Home />}></Route>
-      </Routes>
-    </Router>
-  );
+    return (
+        <div>
+            <Router>
+                <Routes>
+                    <Route exact path='/' element={<Login/>}></Route>
+                    <Route path='/home' element={<Home/>}></Route>
+                </Routes>
+            </Router>
+        </div>
+    );
 }
+
 export default App;
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,3 +1,5 @@
+import React, {useState} from 'react';
+import axios from 'axios';
 import {
     AlipayCircleOutlined,
     LockOutlined,
@@ -13,21 +15,13 @@ import {
     ProFormCheckbox,
     ProFormText,
 } from '@ant-design/pro-components';
-import { message, Space, Tabs } from 'antd';
-import type { CSSProperties } from 'react';
-import { useState } from 'react';
-import { handleLogin } from '../services/handleLogin';
-import { BrowserRouter as Router, 
-  Routes, 
-  Route, 
-  Link,  
-  useNavigate,
-  Navigate
-} from 'react-router-dom';
-  
-  type LoginType = 'phone' | 'account';
-  
-  const iconStyles: CSSProperties = {
+import {message, Space, Tabs} from 'antd';
+import {CSSProperties} from 'react';
+import {useNavigate} from 'react-router-dom';
+
+type LoginType = 'phone' | 'account';
+
+const iconStyles: CSSProperties = {
     marginInlineStart: '16px',
     color: 'rgba(0, 0, 0, 0.2)',
     fontSize: '24px',
@@ -35,136 +29,48 @@ import { BrowserRouter as Router,
     cursor: 'pointer',
 };
 
-
-export default () => {
+const LoginPage: React.FC = () => {
     const [loginType, setLoginType] = useState<LoginType>('phone');
     const navigate = useNavigate();
-    function handleLogin2(values){
-    
-      if(values.mobile === '17878787878' && values.captcha === '123') {
-          message.success('登录成功！');
-      // 处理登录逻辑，例如发送登录请求
-      console.log('测试成功' );
-      
-      navigate('/home');
-      return true;
-      // console.log('登录表单数据:', values);
-      // 跳转到登录逻辑
-      // ...
-      }
-          
-  
-      return new Promise((resolve, reject) => {
-          // 处理登录逻辑，例如发送登录请求
-          // console.log('登录表单数据:', values);
-          // 模拟登录成功
-  
-          resolve(true);
-          // 模拟登录失败
-          // reject(false);
-      });
-  
-  };
 
-
-
+    function handleLogin(values) {
+        console.log('登录表单数据:', values.username, " ", values.password);
+        axios.post('http://47.102.117.173:5000/user/login', {
+            username: values.username,
+            password: values.password
+        }).then(function (response) {
+            message.success("登录成功^_^");
+            navigate('/home');
+            if(!window.localStorage){
+                alert("浏览器不支持localstorage");
+            }else{
+                var storage=window.localStorage;
+                console.log(JSON.stringify(response.data));
+                storage.setItem("body",JSON.stringify(response.data));
+            }
+        }).catch(function (error) {
+            message.error("登录失败-_-");
+        });
+    }
 
     return (
-      <ProConfigProvider hashed={false}>
-        <div style={{ backgroundColor: 'white' }}>
-          <LoginForm
-            onFinish={handleLogin2}
-            logo="https://github.githubassets.com/images/modules/logos_page/Octocat.png"
-            title="Hello"
-            subTitle="github"
-            actions={
-              <Space>
-                其他登录方式
-                <AlipayCircleOutlined style={iconStyles} />
-                <TaobaoCircleOutlined style={iconStyles} />
-                <WeiboCircleOutlined style={iconStyles} />
-              </Space>
-            }
-          >
-            <Tabs
-              centered
-              activeKey={loginType}
-              onChange={(activeKey) => setLoginType(activeKey as LoginType)}
-            >
-              <Tabs.TabPane key={'account'} tab={'账号密码登录'} />
-              <Tabs.TabPane key={'phone'} tab={'手机号登录'} />
-            </Tabs>
-            {loginType === 'account' && (
-              <>
-                <ProFormText
-                  name="username"
-                  fieldProps={{
-                    size: 'large',
-                    prefix: <UserOutlined className={'prefixIcon'} />,
-                  }}
-                  placeholder={'用户名: admin or user'}
-                  rules={[
-                    {
-                      required: true,
-                      message: '请输入用户名!',
-                    },
-                  ]}
-                />
-                <ProFormText.Password
-                  name="password"
-                  fieldProps={{
-                    size: 'large',
-                    prefix: <LockOutlined className={'prefixIcon'} />,
-                  }}
-                  placeholder={'密码: ant.design'}
-                  rules={[
-                    {
-                      required: true,
-                      message: '请输入密码！',
-                    },
-                  ]}
-                />
-              </>
-            )}
-            {loginType === 'phone' && (
-              <>
-                <ProFormText
-                  fieldProps={{
-                    size: 'large',
-                    prefix: <MobileOutlined className={'prefixIcon'} />,
-                  }}
-                  name="mobile"
-                  placeholder={'手机号'}
-                  rules={[
-                    {
-                      required: true,
-                      message: '请输入手机号！',
-                    },
-                    {
-                      pattern: /^1\d{10}$/,
-                      message: '手机号格式错误！',
-                    },
-                  ]}
-                />
-                <ProFormCaptcha
-                  fieldProps={{
-                    size: 'large',
-                    prefix: <LockOutlined className={'prefixIcon'} />,
-                  }}
-                  captchaProps={{
-                    size: 'large',
-                  }}
-                  placeholder={'请输入验证码'}
-                  captchaTextRender={(timing, count) => {
-                    if (timing) {
-                      return `${count} ${'获取验证码'}`;
+        <ProConfigProvider hashed={false}>
+            <div style={{backgroundColor: 'white'}}>
+                <LoginForm
+                    onFinish={handleLogin}
+                    logo="https://github.githubassets.com/images/modules/logos_page/Octocat.png"
+                    title="Hello"
+                    subTitle="github"
+                    actions={
+                        <Space>
+                            其他登录方式
+                            <AlipayCircleOutlined style={iconStyles}/>
+                            <TaobaoCircleOutlined style={iconStyles}/>
+                            <WeiboCircleOutlined style={iconStyles}/>
+                        </Space>
                     }
                 >
-                    <Tabs
-                        centered
-                        activeKey={loginType}
-                        onChange={(activeKey) => setLoginType(activeKey as LoginType)}
-                    >
+                    <Tabs centered activeKey={loginType} onChange={(activeKey) => setLoginType(activeKey as LoginType)}>
                         <Tabs.TabPane key={'account'} tab={'账号密码登录'}/>
                         <Tabs.TabPane key={'phone'} tab={'手机号登录'}/>
                     </Tabs>
@@ -228,13 +134,6 @@ export default () => {
                                 captchaProps={{
                                     size: 'large',
                                 }}
-                                placeholder={'请输入验证码'}
-                                captchaTextRender={(timing, count) => {
-                                    if (timing) {
-                                        return `${count} ${'获取验证码'}`;
-                                    }
-                                    return '获取验证码';
-                                }}
                                 name="captcha"
                                 rules={[
                                     {
@@ -245,27 +144,22 @@ export default () => {
                                 onGetCaptcha={async () => {
                                     message.success('获取验证码成功！验证码为：1234');
                                 }}
+                                captchaTextRender={(timing, count) => (timing ? `${count} 获取验证码` : '获取验证码')}
                             />
                         </>
                     )}
-                    <div
-                        style={{
-                            marginBlockEnd: 24,
-                        }}
-                    >
-                        <ProFormCheckbox noStyle name="autoLogin">
+
+
+                    <div style={{marginBlockEnd: 24}}>
+                        <ProFormCheckbox noStyle name="autoLogin" initialValue={true}>
                             自动登录
                         </ProFormCheckbox>
-                        <a
-                            style={{
-                                float: 'right',
-                            }}
-                        >
-                            忘记密码
-                        </a>
+                        <a style={{float: 'right'}}>忘记密码</a>
                     </div>
                 </LoginForm>
             </div>
         </ProConfigProvider>
     );
 };
+
+export default LoginPage;
